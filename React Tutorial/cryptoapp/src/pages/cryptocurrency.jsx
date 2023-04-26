c52a3d2ac933190f4f0ee4e171409773ff172342
Debmalya Biswas <viperbale.db@gmail.com>
2023-04-26 20:29:58 +0530

    first

--- React Tutorial/cryptoapp/src/pages/cryptocurrency.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const cryptocurrency = () => {
-  return (
-    <div>cryptocurrency</div>
-  )
-}
-
-export default cryptocurrency
--- React Tutorial/cryptoapp/src/pages/exchanges.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const exchanges = () => {
-  return (
-    <div>exchanges</div>
-  )
-}
-
-export default exchanges
